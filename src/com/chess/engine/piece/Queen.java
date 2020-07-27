579578375699197df93788f07c0d2ac7625517f1
Jay Sabale <jay.sable311@gmail.com>
2020-07-27 18:46:40 +0530

    Queen Class Created

---
+++ src/com/chess/engine/piece/Queen.java
@@ -0,0 +1,78 @@
+package com.chess.engine.piece;
+
+import java.util.*;
+
+import com.chess.engine.board.Board;
+import com.chess.engine.board.BoardUtils;
+import com.chess.engine.board.Move;
+import com.chess.engine.board.Tile;
+
+import chess.com.engine.Alliance;
+
+public class Queen extends Piece {
+	
+	private final static int[] CANDIDATE_MOVE_COORDINATES = {-9, -8, -7, -1, 1, 7, 8, 9};
+
+	Queen(int piecePosition, Alliance pieceAlliance) {
+		super(piecePosition, pieceAlliance);
+	}
+
+	@Override
+	public Collection<Move> calculateLegalMove(final  Board board) {
+
+		int candidateDestinationCoordinate;
+		List <Move> legalMoves = new ArrayList<>();
+		
+		for(final int currentCandidateOffset : CANDIDATE_MOVE_COORDINATES)
+		{
+			candidateDestinationCoordinate = this.piecePosition;
+			
+			while(BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)) 
+			{
+				
+				candidateDestinationCoordinate += currentCandidateOffset;
+				
+				if(BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)) 
+				{
+					
+					if(isFirstColumnExclusion(this.piecePosition, currentCandidateOffset) || 
+							isEighthColumnExclusion(this.piecePosition, currentCandidateOffset)) {
+								continue;
+							}
+				
+					final Tile candidateDestinationTile = board.getTile(candidateDestinationCoordinate);
+					
+					if(!candidateDestinationTile.isTileOccupied()) {
+						legalMoves.add(new Move.MajorMove(board, this, candidateDestinationCoordinate));
+					} 
+					
+					else {
+						final Piece pieceAtDestination =  candidateDestinationTile.getPiece();
+						final Alliance pieceAlliance = pieceAtDestination.getPieceAlliance();
+						
+						if(this.pieceAlliance != pieceAlliance) {
+							legalMoves.add(new Move.AttackMove(board, this, candidateDestinationCoordinate, 
+									pieceAtDestination));
+						}
+						
+//						If encounter a blockade, we can't move over it => break
+						break;
+					}
+				}
+			}
+		}
+		
+		return legalMoves;
+	}
+	
+//	Edge Cases:   
+	private static boolean isFirstColumnExclusion(final int currentPosition, final int candidateOffset) {
+		return BoardUtils.FIRST_COLUMN[currentPosition] && (candidateOffset == -9 || candidateOffset == 7 || candidateOffset == -1);
+		
+	}
+	
+	private static boolean isEighthColumnExclusion(final int currentPosition, final int candidateOffset) { 
+		return BoardUtils.EIGHTH_COLUMN[currentPosition] && (candidateOffset == 7 || candidateOffset == -9 || candidateOffset == 1);
+	} 
+	
+}
