b1506a62920debd1adf24ac215d2d33e7fdd0ec2
Jay Sabale <jay.sable311@gmail.com>
2020-07-28 00:15:30 +0530

    Enigma Driver Class Created

---
+++ src/chess/com/engine/Enigma.java
@@ -0,0 +1,13 @@
+package chess.com.engine;
+import com.chess.engine.board.Board;
+
+
+
+//  Driver Class
+public class Enigma {
+	public static void main(String args[]) {
+		Board board = Board.createStandardBoard();
+		
+		System.out.println(board);
+	}
+}
