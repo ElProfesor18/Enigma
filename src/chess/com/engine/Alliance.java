825d37af135c91801684f32dda88c9fd9e3cf5aa
Jay Sabale <jay.sable311@gmail.com>
2020-07-27 18:47:07 +0530

    Updated Alliance Enum
    Added Functions to getAlliance and Direction.

--- src/chess/com/engine/Alliance.java
+++ src/chess/com/engine/Alliance.java
@@ -1,6 +1,40 @@
 package chess.com.engine;
 
 public enum Alliance {
-	WHITE,
-	BLACK
+	WHITE{
+		@Override
+		int getDirection() {
+			return -1;
+		}
+		
+		@Override
+		boolean isWhite() {
+			return true;
+		}
+		
+		@Override
+		boolean isBlack() {
+			return false;
+		}
+	},
+	
+	BLACK{
+		int getDirection() {
+			return 1;
+		}
+		
+		@Override
+		boolean isWhite() {
+			return false;
+		}
+		
+		@Override
+		boolean isBlack() {
+			return true;
+		}
+	};
+	
+	public abstract int getDirection();
+	public abstract boolean isWhite();
+	public abstract boolean isBlack();
 }
