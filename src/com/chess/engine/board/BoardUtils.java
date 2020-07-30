90de4ee70783a5cc5f6d73a83dc45fcecb3d290d
Jay Sabale <jay.sable311@gmail.com>
2020-07-31 02:32:49 +0530

    Updated BoardUtils
    Added the notion of going from Coordinate system to
    Position System and reverse.

--- src/com/chess/engine/board/BoardUtils.java
+++ src/com/chess/engine/board/BoardUtils.java
@@ -1,8 +1,36 @@
 package com.chess.engine.board;
 
+import com.google.common.collect.ImmutableMap;
+
+import java.util.*;
+
 public class BoardUtils {
     public static final int NUM_TILES = 64;
     public static final int NUM_TILES_PER_ROW = 8;
+    public static final String[] ALGEBRAIC_NOTATION = initializeAlgebraicNotation();
+    public static final Map <String, Integer> POSITION_TO_COORDINATE = initializePositionToCoordinateMap();
+
+    public static String [] initializeAlgebraicNotation(){
+        return new String[] {
+                "a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8",
+                "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7",
+                "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6",
+                "a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5",
+                "a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4",
+                "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3",
+                "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2",
+                "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1"
+        };
+    }
+
+    public static Map <String , Integer> initializePositionToCoordinateMap(){
+         final Map <String, Integer> positionToCoordinate = new HashMap<>();
+         for(int i=0; i<BoardUtils.NUM_TILES; i++) {
+             positionToCoordinate.put(ALGEBRAIC_NOTATION[i], i);
+         }
+
+         return ImmutableMap.copyOf(positionToCoordinate);
+    }
 
     private BoardUtils() {
         throw new RuntimeException("You cannot instantiate me!");
@@ -30,6 +58,13 @@ public class BoardUtils {
         } while(rowNumber % NUM_TILES_PER_ROW != 0);
 
         return row;
+    }
+
+    public static  int getCoordinateAtPosition(final String position){
+        return POSITION_TO_COORDINATE.get(position);
+    }
+    public static String getPositionAtCoordinate(int coordinate) {
+        return ALGEBRAIC_NOTATION[coordinate];
     }
 
     public static final boolean[] FIRST_COLUMN = initColumn(0);
