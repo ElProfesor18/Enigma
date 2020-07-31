22b1787d970b8db207a7c98ba30143b85ee304e4
Jay Sabale <jay.sable311@gmail.com>
2020-07-31 16:54:18 +0530

    Update certain subclasses of Pieces Class
    Superseded the AttackMove class with MajorAttackMove

--- src/com/chess/engine/pieces/King.java
+++ src/com/chess/engine/pieces/King.java
@@ -44,7 +44,7 @@ public class King  extends Piece{
                     final Alliance pieceAlliance = pieceAtDestination.getPieceAlliance();
 
                     if(this.pieceAlliance != pieceAlliance) {
-                        legalMoves.add(new Move.AttackMove(board, this, candidateDestinationCoordinate,
+                        legalMoves.add(new Move.MajorAttackMove(board, this, candidateDestinationCoordinate,
                                 pieceAtDestination));
                     }
                 }
--- src/com/chess/engine/pieces/Knight.java
+++ src/com/chess/engine/pieces/Knight.java
@@ -49,7 +49,7 @@ public class Knight extends Piece {
                     final Piece pieceAtDestination = candidateDestinationTile.getPiece();
                     final Alliance pieceAlliance = pieceAtDestination.getPieceAlliance();
                     if (this.pieceAlliance != pieceAlliance) {
-                        legalMoves.add(new Move.AttackMove(board,
+                        legalMoves.add(new Move.MajorAttackMove(board,
                                         this,
                                         candidateDestinationCoordinate,
                                         pieceAtDestination));
